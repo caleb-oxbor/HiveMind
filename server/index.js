78533fc83a2ec1b8721e589b00d38970933d9e9d
Alina Garib <alinagrb@gmail.com>
2024-10-08 13:48:11 -0400

    database and server changes

--- server/index.js
+++ server/index.js
@@ -1,12 +1,19 @@
  const express = require("express");
  const app = express();
  const cors = require("cors");
+ const pool = require("./db");
 
  //middleware
  app.use(cors());
 
  //get data from client
  app.use(express.json());
+
+ //ROUTES
+
+ //create accouunt 
+
+ //get account 
 
  app.listen(5000, () => {
     console.log("server has started on port 5000");
