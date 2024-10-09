9a7bd0140c6d8b36f4410d5753c9aec12b93f886
Caleb Oxborough <caleb.oxborough@ufl.edu>
2024-10-09 09:50:50 -0400

    added routes to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
+// npm install react-router-dom
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Welcome from './components/welcome';
+import CreateAccount from './components/createaccount';
+import Login from './components/login';
 
 function App() {
   return (
     <div className="App">
+
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/create-account" element={<CreateAccount />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
