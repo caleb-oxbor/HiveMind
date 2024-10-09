7ea1af72ffb55d9c6d280b0b41f5b47d61a91ae1
Caleb Oxborough <caleb.oxborough@ufl.edu>
2024-10-09 15:41:27 -0400

    cleanup debug statements and old, commented code

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,8 @@
-// npm install react-router-dom
-//import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Welcome from './components/Welcome';
-import CreateAccount from './components/CreateAccount';
-import Login from './components/LogIn';
 import { Link } from 'react-router-dom';
 
-// function App() {
-//   return (
-//     <Router>
-//       <div className="App">
-
-//         <Routes>
-//           <Route path="/" element={<Welcome />} />
-//           <Route path="/create-account" element={<CreateAccount />} />
-//           <Route path="/login" element={<Login />} />
-//         </Routes>
-
-//       </div>
-//     </Router>
-//   );
-// }
-//import React, { Fragment, useState } from "react";
-
-//components
-
-// function App() {
-//   const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-//   const setAuth = (boolean) => {
-//     setIsAuthenticated(boolean);
-//   };
-  
-//   return (
-//     <div className="App">APP TEST</div>
-//   );
-// }
-
 function App() {
-  console.log("Welcome component rendered"); // Debug line
   return (
     <div>
       <h1>Welcome to HiveMind!</h1>
--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Welcome() {
-    console.log("Welcome component rendered"); // Debug line
   return (
     <div>
       <h1>Welcome to HiveMind!</h1>
@@ -16,20 +15,5 @@ function Welcome() {
     </div>
   );
 }
-
-// function Welcome() {
-//     console.log("Welcome component rendered"); // Debug line
-//   return (
-//     <div>
-//       <h1>Welcome to HiveMind!</h1>
-//       <Link to="/create-account">
-//         <button>Create Account</button>
-//       </Link>
-//       <Link to="/login">
-//         <button>Login</button>
-//       </Link>
-//     </div>
-//   );
-// }
 
 export default Welcome;
