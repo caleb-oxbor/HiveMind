1341cfb92712050a4f677257dfb9492b89b569d2
Caleb Oxborough <caleb.oxborough@ufl.edu>
2024-10-16 20:58:48 -0400

    commented out but kept supabase demo so groupmates can see for themselves

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -7,33 +7,33 @@ function Welcome() {
   const [fetchError, setFetchError] = useState(null)
   const [universities, setUnis] = useState(null)
 
-  useEffect(() => {
-    const fetchUnis = async () => {
-      const {data, error} = await supabase
-      .from('universities')
-      .select()
+  // useEffect(() => {
+  //   const fetchUnis = async () => {
+  //     const {data, error} = await supabase
+  //     .from('universities')
+  //     .select()
 
-      if (error) {
-        setFetchError('Could not fetch universities')
-        setUnis(null)
-        console.log(error)
-      }
-      if (data) {
-        setUnis(data)
-        setFetchError(null)
-      }
-    }
+  //     if (error) {
+  //       setFetchError('Could not fetch universities')
+  //       setUnis(null)
+  //       console.log(error)
+  //     }
+  //     if (data) {
+  //       setUnis(data)
+  //       setFetchError(null)
+  //     }
+  //   }
 
-    fetchUnis()
+  //   fetchUnis()
 
-  }, [])
+  // }, [])
   
   return (
     <div className="fullscreen-background"> 
       <h1 className="font-tiny5 font-bold text-center text-white text-8xl mt-10 mb-1 heading-shadow">Welcome To HiveMind!</h1>
       <h3 className="font-dotgothic text-center text-white text-3xl my-5">Click below to navigate</h3>
 
-      <div className="supabase testing">
+      {/* <div className="supabase testing">
         {fetchError && (<p>{fetchError}</p>)}
         {universities && (
           <div className="uni printage">
@@ -42,7 +42,7 @@ function Welcome() {
             ))}
           </div>
         )}
-      </div>
+      </div> */}
 
       <Link to="/register">
         <button className="font-dotgothic custom-button">Create Account</button>
